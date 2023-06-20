a35cbe18c9357e5fa819c599ad119b6da5278685
周东 <zhoudong@zz-med.com>
2023-06-20 09:18:28 +0800

    修改文件测试

---
+++ utils/toolsrun/dayforhub.go
@@ -0,0 +1,13 @@
+package testtools
+
+import "fmt"
+
+func main() {
+	fmt.Print("for hub verify")
+	for true {
+		fmt.Println("brains test")
+		break
+	}
+
+	fmt.Println("brains need 3 month so use this project")
+}
